0fd2a5eb18703635be659c2b0ef9d6ed8cd4e8b1
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-07-28 11:47:05 +0900

    feat: 좋아요 등록/해제 mock 조회 API 생성

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/like/LikeV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/like/LikeV1Controller.java
@@ -1,9 +1,13 @@
 package com.loopers.interfaces.api.like;
 
 import com.loopers.interfaces.api.ApiResponse;
+import com.loopers.interfaces.api.like.LikeV1Dto.Get;
+import com.loopers.interfaces.api.like.LikeV1Dto.Get.Contents;
 import com.loopers.interfaces.api.like.LikeV1Dto.Register.Response;
 import com.loopers.interfaces.api.like.LikeV1Dto.Unregister;
+import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -18,11 +22,11 @@ public class LikeV1Controller {
   public ApiResponse<LikeV1Dto.Register.Response> register(
       @RequestHeader("X-User-Id") String userId,
       @PathVariable Long productId) {
-   return ApiResponse.success(new Response(
-       userId,
-       productId,
-       true
-   ));
+    return ApiResponse.success(new Response(
+        userId,
+        productId,
+        true
+    ));
   }
 
   @DeleteMapping("/{productId}")
@@ -30,9 +34,19 @@ public class LikeV1Controller {
       @RequestHeader("X-User-Id") String userId,
       @PathVariable Long productId) {
     return ApiResponse.success(new Unregister.Response(
-       userId,
-       productId,
-       false));
+        userId,
+        productId,
+        false));
+  }
+
+  @GetMapping()
+  public ApiResponse<LikeV1Dto.Get.Response> get(
+      @RequestHeader("X-User-Id") String userId) {
+    return ApiResponse.success(new Get.Response(
+        List.of(new Contents(1L,"상품1"),
+            new Contents(1L,"상품1")),
+        1,1,1,1
+    ));
   }
 
 }
--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/like/LikeV1Dto.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/like/LikeV1Dto.java
@@ -1,5 +1,7 @@
 package com.loopers.interfaces.api.like;
 
+import java.util.List;
+
 public class LikeV1Dto {
 
   class Register {
@@ -7,6 +9,15 @@ public class LikeV1Dto {
   }
   class Unregister {
     record Response(String userId, Long productId, boolean status) {}
+  }
+
+  class Get {
+    record Response(List<Contents> contents,
+                    int page,
+                    int size,
+                    int totalElements,
+                    int totalPages) {}
+    record Contents(Long productId, String productName) {}
   }
 
 }
