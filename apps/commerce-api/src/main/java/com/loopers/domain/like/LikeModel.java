543322111cecd85b612b92860615a38edc65b99f
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-07-29 16:12:18 +0900

    test: 계정 또는 상품이 존재하지 않는 경우 테스트

--- apps/commerce-api/src/main/java/com/loopers/domain/like/LikeModel.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/like/LikeModel.java
@@ -1,16 +1,18 @@
 package com.loopers.domain.like;
 
 import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import java.time.ZonedDateTime;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Getter
 @Table(name = "like")
 public class LikeModel extends BaseEntity {
@@ -18,4 +20,24 @@ public class LikeModel extends BaseEntity {
   private String memberId;
   private Long productId;
 
+  public static LikeModel register(String memberId, Long productId) {
+    return new LikeModel(memberId, productId);
+  }
+
+  private LikeModel(String memberId, Long productId) {
+    if (memberId == null) {
+      throw new CoreException(ErrorType.BAD_REQUEST, "계정 아이디는 필수입니다.");
+    }
+    if (productId == null) {
+      throw new CoreException(ErrorType.BAD_REQUEST, "상품 아이디는 필수입니다.");
+    }
+
+    this.memberId = memberId;
+    this.productId = productId;
+  }
+
+  public ZonedDateTime cancel() {
+    this.delete();
+    return this.getDeletedAt();
+  }
 }
