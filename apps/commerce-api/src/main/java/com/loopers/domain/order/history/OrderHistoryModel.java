e0e860b73b560acabd932d053c5e6d80df519f0f
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-07-30 15:20:47 +0900

    test: 주문 아이템 단위 테스트

--- apps/commerce-api/src/main/java/com/loopers/domain/order/history/OrderHistoryModel.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/order/history/OrderHistoryModel.java
@@ -7,12 +7,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Table(name = "order_history")
 @Getter
 public class OrderHistoryModel extends BaseEntity {
@@ -27,8 +27,20 @@ public class OrderHistoryModel extends BaseEntity {
   @Column(nullable = false, length = 25)
   private String status;
 
+  @Column(nullable = false, length = 200)
   private String address;
 
   @Column(columnDefinition = "TEXT")
   private String memo;
+
+  @Builder
+  public OrderHistoryModel(Long orderId, String orderNumber, String memberId, String status, String address,
+                           String memo) {
+    this.orderId = orderId;
+    this.orderNumber = orderNumber;
+    this.memberId = memberId;
+    this.status = status;
+    this.address = address;
+    this.memo = memo;
+  }
 }
