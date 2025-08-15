b6cbcc5ddf3584c8c6d22af4752232891b6eff91
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-08-15 12:06:46 +0900

    chor: k6 테스트 코드

--- docker/scripts/search.js
+++ docker/scripts/search.js
@@ -3,12 +3,12 @@ import { check, sleep } from 'k6';
 
 // 부하 설정
 export const options = {
-    vus: 1, // 동시에 실행할 가상 사용자 수
-    duration: '30s', // 테스트 시간
+    vus: 10, // 동시에 실행할 가상 사용자 수
+    duration: '60s', // 테스트 시간
 };
 
 export default function () {
-    const url = 'http://host.docker.internal:8080/api/v1/products'; // 엔드포인트 주소
+    const url = 'http://host.docker.internal:8080/api/v1/products?currentPage=0&perPage=10&sort=LIKES_DESC'; // 엔드포인트 주소
     const res = http.get(url);
 
     // 응답 검증
