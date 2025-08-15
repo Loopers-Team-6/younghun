394b5b8f10547a551c5b9186df1a3f22e9923912
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-08-15 13:27:56 +0900

    인덱스 추가 (#52)
    * chor:k6 기본 스크립트 작성 (0,10)
    
    * feat: 인덱스 설정
    
    * feat: 브랜드 100만건
    
    * chor: 데이터 세팅
    
    * chor: 모델 수정
    
    * chor: 데이터 세팅 다시..
    
    * chor: k6 테스트 코드
    
    * chor: influx, 테스트 준비 완료
    
    * chor: 오름 차순 정렬

--- modules/jpa/src/main/resources/data.sql
+++ modules/jpa/src/main/resources/data.sql
@@ -1,17 +1,19 @@
--- MySQL을 기준으로 작성된 100만 개의 더미 데이터 생성 스크립트입니다.
--- MySQL 8.0 이상 버전에서 작동합니다.
-
--- 재귀 쿼리의 최대 깊이를 1000000으로 설정합니다.
--- 이 값은 현재 세션에만 적용됩니다.
+-- MySQL 8.0 이상 버전에서 작동
 SET @@cte_max_recursion_depth = 1000000;
 
-
--- brand 데이터 삽입
-INSERT INTO brand (user_id, name, created_at, updated_at) VALUES
-                                                              ('user1', '루프스', NOW(), NOW()),
-                                                              ('user2', '소마웨어', NOW(), NOW()),
-                                                              ('user3', '에이블코어', NOW(), NOW()),
-                                                              ('user4', '하이로닉스', NOW(), NOW());
+-- 10만 개의 brand 더미 데이터 삽입
+INSERT INTO brand (user_id, name, created_at, updated_at)
+WITH RECURSIVE numbers AS (
+    SELECT 1 AS n
+    UNION ALL
+    SELECT n + 1 FROM numbers WHERE n < 100000
+)
+SELECT
+    CONCAT('user', n) AS user_id,
+    CONCAT('Brand Name ', n) AS name,
+    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS created_at,
+    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS updated_at
+FROM numbers;
 
 -- 100만 개의 product 더미 데이터 삽입
 INSERT INTO product (brand_id, name, price, description, created_at, updated_at)
@@ -21,12 +23,12 @@ WITH RECURSIVE numbers AS (
     SELECT n + 1 FROM numbers WHERE n < 1000000
 )
 SELECT
-    FLOOR(1 + RAND() * 4) AS brand_id,
+    FLOOR(1 + RAND() * 100000) AS brand_id, -- 브랜드는 1~10만 랜덤
     CONCAT('Product Name ', n) AS name,
-    FLOOR(100 + RAND() * 1901) * 100 AS price,
+    FLOOR(100 + RAND() * 2000000) * 100 AS price,
     CONCAT('Description for Product ', n) AS description,
-    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 10 * 24 * 3600), NOW()) AS created_at,
-    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 10 * 24 * 3600), NOW()) AS updated_at
+    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS created_at,
+    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS updated_at
 FROM numbers;
 
 -- 100만 개의 stock 더미 데이터 삽입
@@ -38,9 +40,9 @@ WITH RECURSIVE numbers AS (
 )
 SELECT
     n AS product_id,
-    FLOOR(10 + RAND() * 491) AS stock,
-    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 10 * 24 * 3600), NOW()) AS created_at,
-    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 10 * 24 * 3600), NOW()) AS updated_at
+    FLOOR(10 + RAND() * 2000000) AS stock,
+    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS created_at,
+    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS updated_at
 FROM numbers;
 
 -- 100만 개의 product_status 더미 데이터 삽입
@@ -52,7 +54,7 @@ WITH RECURSIVE numbers AS (
 )
 SELECT
     n AS product_id,
-    FLOOR(RAND() * 3001) AS like_count,
-    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 10 * 24 * 3600), NOW()) AS created_at,
-    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 10 * 24 * 3600), NOW()) AS updated_at
+    FLOOR(RAND() * 2000000) AS like_count,
+    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS created_at,
+    TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS updated_at
 FROM numbers;
