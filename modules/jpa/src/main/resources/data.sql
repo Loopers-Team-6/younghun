9455f749937b60bf1a6777ed85cd8ceb32d25af2
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-08-15 12:06:32 +0900

    chor: 데이터 세팅 다시..

--- modules/jpa/src/main/resources/data.sql
+++ modules/jpa/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- MySQL 8.0 이상 버전에서 작동
-SET @@cte_max_recursion_depth = 100000;
+SET @@cte_max_recursion_depth = 1000000;
 
 -- 10만 개의 brand 더미 데이터 삽입
 INSERT INTO brand (user_id, name, created_at, updated_at)
@@ -15,15 +15,15 @@ SELECT
     TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS updated_at
 FROM numbers;
 
--- 10만 개의 product 더미 데이터 삽입
+-- 100만 개의 product 더미 데이터 삽입
 INSERT INTO product (brand_id, name, price, description, created_at, updated_at)
 WITH RECURSIVE numbers AS (
     SELECT 1 AS n
     UNION ALL
-    SELECT n + 1 FROM numbers WHERE n < 100000
+    SELECT n + 1 FROM numbers WHERE n < 1000000
 )
 SELECT
-    FLOOR(1 + RAND() * 100000) AS brand_id, -- 브랜드 1~10만 랜덤
+    FLOOR(1 + RAND() * 100000) AS brand_id, -- 브랜드는 1~10만 랜덤
     CONCAT('Product Name ', n) AS name,
     FLOOR(100 + RAND() * 2000000) * 100 AS price,
     CONCAT('Description for Product ', n) AS description,
@@ -31,12 +31,12 @@ SELECT
     TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS updated_at
 FROM numbers;
 
--- 10만 개의 stock 더미 데이터 삽입
+-- 100만 개의 stock 더미 데이터 삽입
 INSERT INTO stock (product_id, stock, created_at, updated_at)
 WITH RECURSIVE numbers AS (
     SELECT 1 AS n
     UNION ALL
-    SELECT n + 1 FROM numbers WHERE n < 100000
+    SELECT n + 1 FROM numbers WHERE n < 1000000
 )
 SELECT
     n AS product_id,
@@ -45,12 +45,12 @@ SELECT
     TIMESTAMPADD(SECOND, -FLOOR(RAND() * 300 * 24 * 3600), NOW()) AS updated_at
 FROM numbers;
 
--- 10만 개의 product_status 더미 데이터 삽입
+-- 100만 개의 product_status 더미 데이터 삽입
 INSERT INTO product_status (product_id, like_count, created_at, updated_at)
 WITH RECURSIVE numbers AS (
     SELECT 1 AS n
     UNION ALL
-    SELECT n + 1 FROM numbers WHERE n < 100000
+    SELECT n + 1 FROM numbers WHERE n < 1000000
 )
 SELECT
     n AS product_id,
