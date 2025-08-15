f864a41c24fcd2f4c0e186e975810e116252843a
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-08-15 13:26:14 +0900

    chor: 오름 차순 정렬

--- apps/commerce-api/src/main/resources/sql/create_index_like_brand_prdocut.sql
+++ apps/commerce-api/src/main/resources/sql/create_index_like_brand_prdocut.sql
@@ -1,2 +1,2 @@
 CREATE INDEX idx_brand_id_and_id ON product (brand_id, id);
-CREATE INDEX idx_product_id_and_like_count ON product_status (product_id, like_count DESC);
+CREATE INDEX idx_product_id_and_like_count ON product_status (product_id DESC, like_count DESC);
