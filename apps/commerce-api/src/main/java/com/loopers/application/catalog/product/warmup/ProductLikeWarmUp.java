c27fe46dde5770d3544aa0a5be7e5833f07dba06
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-08-17 16:42:12 +0900

    refactor: import 정리

--- apps/commerce-api/src/main/java/com/loopers/application/catalog/product/warmup/ProductLikeWarmUp.java
+++ apps/commerce-api/src/main/java/com/loopers/application/catalog/product/warmup/ProductLikeWarmUp.java
@@ -1,19 +1,8 @@
 package com.loopers.application.catalog.product.warmup;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.loopers.application.catalog.product.ProductCommand;
-import com.loopers.application.catalog.product.SortOption;
-import com.loopers.domain.catalog.product.ProductProjection;
-import com.loopers.domain.catalog.product.ProductRepository;
-import com.loopers.domain.catalog.product.cache.ProductCacheRepository;
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
