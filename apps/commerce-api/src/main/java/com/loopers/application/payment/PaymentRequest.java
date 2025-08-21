9caa39db48548f1c2d454931a291537447997be9
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-08-21 13:25:14 +0900

    refactor: 일단 테스트 되게 만들어놓음.

--- apps/commerce-api/src/main/java/com/loopers/application/payment/PaymentRequest.java
+++ apps/commerce-api/src/main/java/com/loopers/application/payment/PaymentRequest.java
@@ -3,11 +3,16 @@ package com.loopers.application.payment;
 public record PaymentRequest(
     String orderId,
     PaymentTool paymentTool,
+    String cardType,
+    String cardNo,
     Long amount,
     String callbackUrl
 ) {
   public PaymentRequest(PaymentGatewayCommand command, String callbackUrl) {
-    this(command.orderId(), PaymentTool.valueOf(command.paymentTool().name()), command.amount().longValue(), callbackUrl);
+    this(command.orderId(), PaymentTool.valueOf(command.paymentTool().name()),
+        "KB",
+        "1234-1234-1234-1234",
+        command.amount().longValue(), callbackUrl);
   }
 
   enum PaymentTool {
