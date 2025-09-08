093d25f31e6b699763829cfbcb3a77a3602ed450
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-09-08 16:16:51 +0900

    refactor: 하나의 컨슈머로 전달

--- apps/commerce-streamer/src/main/java/com/loopers/interfaces/consumer/MetricsKafkaConsumer.java
+++ apps/commerce-streamer/src/main/java/com/loopers/interfaces/consumer/MetricsKafkaConsumer.java
@@ -7,23 +7,23 @@ import com.loopers.support.shared.Message;
 import com.loopers.support.shared.MessageConvert;
 import java.util.List;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MetricsKafkaConsumer {
-  private final MessageConvert convert;
   private final MetricsStrategyFactory factory;
-
-  public MetricsKafkaConsumer(MessageConvert convert, MetricsStrategyFactory factory) {
-    this.convert = convert;
+  public MetricsKafkaConsumer(MetricsStrategyFactory factory) {
     this.factory = factory;
   }
 
   @KafkaListener(
       topics = {
+          "${aggregate-kafka.stock.topic-name}",
           "${aggregate-kafka.like.topic-name}",
+          "${aggregate-kafka.views.topic-name}"
       },
       groupId = "${aggregate-kafka.group-id}",
       containerFactory = KafkaConfig.BATCH_LISTENER
@@ -34,44 +34,10 @@ public class MetricsKafkaConsumer {
   ) {
 
     for (ConsumerRecord<String, String> message : messages) {
-      factory.getStrategy(MetricsMethod.LIKES).process(message.value());
+      factory.getStrategy(MetricsMethod.find(message.topic())).process(message.value());
     }
 
     acknowledgment.acknowledge();
   }
 
-  @KafkaListener(
-      topics = {
-          "${aggregate-kafka.stock.topic-name}",
-      },
-      groupId = "${aggregate-kafka.group-id}",
-      containerFactory = KafkaConfig.BATCH_LISTENER
-  )
-  public void aggregateStockListener(
-      List<ConsumerRecord<String, String>> messages,
-      Acknowledgment acknowledgment
-  ) {
-
-    for (ConsumerRecord<String, String> message : messages) {
-      factory.getStrategy(MetricsMethod.SALES).process(message.value());
-    }
-    acknowledgment.acknowledge();
-  }
-
-  @KafkaListener(
-      topics = {
-          "${aggregate-kafka.views.topic-name}"
-      },
-      groupId = "${aggregate-kafka.group-id}",
-      containerFactory = KafkaConfig.BATCH_LISTENER
-  )
-  public void aggregateViewsListener(
-      List<ConsumerRecord<String, String>> messages,
-      Acknowledgment acknowledgment
-  ) {
-    for (ConsumerRecord<String, String> message : messages) {
-      factory.getStrategy(MetricsMethod.VIEWS).process(message.value());
-    }
-    acknowledgment.acknowledge();
-  }
 }
