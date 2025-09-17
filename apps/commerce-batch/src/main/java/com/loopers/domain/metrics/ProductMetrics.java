411e87a7b18fe39a5dbf80407101649905a5ce52
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-09-17 13:59:00 +0900

    배치 설정 (#93)
    * chor: 포트 번호 변경
    
    * feat: 'product_metrics' 테이블 접근
    
    * chor: batch 테이블 init 추가

---
+++ apps/commerce-batch/src/main/java/com/loopers/domain/metrics/ProductMetrics.java
@@ -0,0 +1,56 @@
+package com.loopers.domain.metrics;
+
+import com.loopers.domain.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "product_metrics")
+public class ProductMetrics extends BaseEntity {
+  private Long productId;
+  private LocalDate date;
+  private Long views;
+  private Long sales;
+  private Long likes;
+
+  public Long getProductId() {
+    return productId;
+  }
+
+  public void setProductId(Long productId) {
+    this.productId = productId;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public Long getViews() {
+    return views;
+  }
+
+  public void setViews(Long views) {
+    this.views = views;
+  }
+
+  public Long getSales() {
+    return sales;
+  }
+
+  public void setSales(Long sales) {
+    this.sales = sales;
+  }
+
+  public Long getLikes() {
+    return likes;
+  }
+
+  public void setLikes(Long likes) {
+    this.likes = likes;
+  }
+}
