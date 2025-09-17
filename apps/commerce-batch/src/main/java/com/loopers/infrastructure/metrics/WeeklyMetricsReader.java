3aa12793271dc18f167a21e8900869cc379b85d0
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-09-18 03:28:58 +0900

    feat: 빌더 형식으로 변경

--- apps/commerce-batch/src/main/java/com/loopers/infrastructure/metrics/WeeklyMetricsReader.java
+++ apps/commerce-batch/src/main/java/com/loopers/infrastructure/metrics/WeeklyMetricsReader.java
@@ -1,16 +1,12 @@
 package com.loopers.infrastructure.metrics;
 
 import com.loopers.domain.metrics.ProductMetrics;
-import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-import javax.sql.DataSource;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.data.RepositoryItemReader;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,12 +24,13 @@ public class WeeklyMetricsReader {
   @Bean
   @StepScope
   public ItemReader<ProductMetrics> weaklyAggregateReader(@Value("#{jobParameters['date']}") String date) {
-    RepositoryItemReader<ProductMetrics> reader = new RepositoryItemReader<>();
-    reader.setRepository(productMetricsRepository);
-    reader.setMethodName("findByDate");
-    reader.setArguments(List.of(LocalDate.parse(date))); // JobParameters 활용
-    reader.setPageSize(1000); // 페이징 단위
-    reader.setSort(Map.of("id", Sort.Direction.ASC));
-    return reader;
+    return new RepositoryItemReaderBuilder<ProductMetrics>()
+        .name("weaklyAggregateReader")
+        .repository(productMetricsRepository)
+        .methodName("findByDate")
+        .arguments(List.of(LocalDate.parse(date)))
+        .pageSize(1000)
+        .sorts(Map.of("id", Sort.Direction.ASC))
+        .build();
   }
 }
